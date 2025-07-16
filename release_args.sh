48c1fb00b786b5d14666cab7f1281bcb3e004bf8
Niklas <niklas.hinze@cloudogu.com>
2025-07-16 08:25:44 +0200

    #5 Extend release_args with nginx

--- release_args.sh
+++ release_args.sh
@@ -3,6 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+componentTemplateFile=k8s/helm/component-patch-tpl.yaml
+
 # this function will be sourced from release.sh and be called from release_functions.sh
 update_versions_modify_files() {
   newReleaseVersion="${1}"
@@ -11,6 +13,22 @@ update_versions_modify_files() {
 
   ./.bin/yq -i ".controllerManager.manager.image.tag = \"${newReleaseVersion}\"" "${valuesYAML}"
   ./.bin/yq -i ".values.images.supportArchiveOperator |= sub(\":(([0-9]+)\.([0-9]+)\.([0-9]+)((?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))|(?:\+[0-9A-Za-z-]+))?)\", \":${newReleaseVersion}\")" "${componentPatchTplYAML}"
+
+  local nginxRegistry
+  local nginxRepo
+  local nginxTag
+  nginxRegistry=$(yq '.webserver.image.registry' < "${valuesYAML}")
+  nginxRepo=$(yq 'webserver.image.repository' < "${valuesYAML}")
+  nginxTag=$(yq 'webserver.image.tag' < "${valuesYAML}")
+  setAttributeInComponentPatchTemplate ".values.images.kubectl" "${nginxRegistry}/${nginxRepo}:${nginxTag}"
+
+}
+
+setAttributeInComponentPatchTemplate() {
+  local key="${1}"
+  local value="${2}"
+
+  yq -i "${key} = \"${value}\"" "${componentTemplateFile}"
 }
 
 update_versions_stage_modified_files() {
