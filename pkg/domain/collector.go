f7d489af98e8059358ce264973970f04baa010dc
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-08-21 14:25:10 +0200

    #14 remove SecretList out of CollectorUnionDataType

--- pkg/domain/collector.go
+++ pkg/domain/collector.go
@@ -2,7 +2,6 @@ package domain
 
 import (
 	libapi "github.com/cloudogu/k8s-support-archive-lib/api/v1"
-	v1 "k8s.io/api/core/v1"
 )
 
 type CollectorType string
@@ -27,5 +26,5 @@ func (c CollectorType) GetConditionType() string {
 }
 
 type CollectorUnionDataType interface {
-	PodLog | VolumeInfo | v1.SecretList | SecretYaml
+	PodLog | VolumeInfo | SecretYaml
 }
