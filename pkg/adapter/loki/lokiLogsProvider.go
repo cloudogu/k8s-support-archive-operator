abc49c46de71794dea0ddb3cd2b96c3fc503d425
Sven Gürtler <sven.guertler@cloudogu.com>
2025-09-08 16:37:53 +0200

    WIP: impl query time window iterator

--- pkg/adapter/loki/lokiLogsProvider.go
+++ pkg/adapter/loki/lokiLogsProvider.go
@@ -2,28 +2,91 @@ package loki
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 )
 
+const maxQueryTimeWindowInDays = 30
+
 type lokiLogsProvider struct {
+	httpAPIUrl string
 	httpClient *http.Client
 }
 
-func NewLokiLogsProvider(httpClient *http.Client) *lokiLogsProvider {
+func NewLokiLogsProvider(httpClient *http.Client, httpAPIUrl string) *lokiLogsProvider {
 	return &lokiLogsProvider{
-		httpClient,
+		httpAPIUrl: httpAPIUrl,
+		httpClient: httpClient,
 	}
 }
 
-func (lp *lokiLogsProvider) getValuesOfLabel(ctx context.Context, start, end time.Time, label string) ([]string, error) {
-	_, err := lp.httpClient.Get("http://example.com")
-	if err != nil {
-		return []string{}, err
+func (lp *lokiLogsProvider) getValuesOfLabel(ctx context.Context, startTime, endTime time.Time, label string) ([]string, error) {
+	var reqStartTime int64
+	var resEndTime int64 = startTime.UnixNano()
+	var hasNext = true
+	for hasNext {
+		reqStartTime, resEndTime, hasNext = nextTimeWindow(resEndTime, endTime.UnixNano(), maxQueryTimeWindowInDays)
+		_, err := lp.doLokiHttpQuery(reqStartTime, resEndTime)
+		if err != nil {
+			return []string{}, err
+		}
 	}
+
 	return []string{}, nil
 }
 
 func (lp *lokiLogsProvider) getLogs(ctx context.Context, start, end time.Time, namespace string, kind string) ([]string, error) {
 	return []string{}, nil
+}
+
+func (lp *lokiLogsProvider) doLokiHttpQuery(startTimeInUnixNano int64, endTimeInUnixNano int64) ([]string, error) {
+
+	url, err := buildLokiQuery(lp.httpAPIUrl, startTimeInUnixNano, endTimeInUnixNano)
+	if err != nil {
+		return []string{}, err
+	}
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return []string{}, err
+	}
+	_, err = lp.httpClient.Do(request)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return []string{}, nil
+}
+
+func buildLokiQuery(httpAPIUrl string, startTimeInUnixNano int64, endTimeInUnixNano int64) (string, error) {
+	baseUrl, err := url.Parse(httpAPIUrl)
+	if err != nil {
+		return "", err
+	}
+
+	baseUrl = baseUrl.JoinPath("/loki/api/v1/query_range")
+
+	params := baseUrl.Query()
+	params.Set("start", fmt.Sprintf("%d", startTimeInUnixNano))
+	params.Set("end", fmt.Sprintf("%d", endTimeInUnixNano))
+
+	baseUrl.RawQuery = params.Encode()
+
+	return baseUrl.String(), nil
+}
+
+func nextTimeWindow(startTimeInNanoSec int64, maxEndTimeInNanoSec int64, maxTimeWindowInDays int) (int64, int64, bool) {
+	maxTimeWindowInNanoSec := time.Hour.Nanoseconds() * 24 * int64(maxTimeWindowInDays)
+	timeWindowEndInNanoSec := minInt64(startTimeInNanoSec+maxTimeWindowInNanoSec, maxEndTimeInNanoSec)
+	hasNext := timeWindowEndInNanoSec < maxEndTimeInNanoSec
+	return startTimeInNanoSec, timeWindowEndInNanoSec, hasNext
+}
+
+func minInt64(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
 }
--- pkg/adapter/loki/lokiLogsProvider_test.go
+++ pkg/adapter/loki/lokiLogsProvider_test.go
@@ -4,27 +4,147 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+type httpServerCall struct {
+	reqStartTime int64
+	reqEndTime   int64
+	reqError     error
+}
 
 func TestLokiLogsProvider(t *testing.T) {
 
-	t.Run("should call REST API once because duration is less than the 30d1h limit", func(t *testing.T) {
+	t.Run("should call API one time if endTime == startTime + maxTimeWindow", func(t *testing.T) {
+		startTime := time.Now()
+		endTime := startTime.AddDate(0, 0, maxQueryTimeWindowInDays)
+
 		var callCount int
+		var reqStartTime, reqEndTime int64
+		var reqError error
+
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			callCount += 1
+			reqStartTime, reqEndTime, reqError = parseStartAndEndTime(r)
+			require.NoError(t, reqError)
 		}))
 		defer server.Close()
 
-		lokiLogsPrv := NewLokiLogsProvider(server.Client())
-		startTime := time.Now()
-		endTime := startTime.AddDate(0, 0, 30)
+		lokiLogsPrv := NewLokiLogsProvider(server.Client(), server.URL)
+
 		_, _ = lokiLogsPrv.getValuesOfLabel(context.TODO(), startTime, endTime, "aKind")
 
 		assert.Equal(t, 1, callCount)
 
+		assert.Equal(t, startTime.UnixNano(), reqStartTime)
+		assert.Equal(t, endTime.UnixNano(), reqEndTime)
 	})
+
+	t.Run("should call API two times if endTime == startTime + 2*maxTimeWindow ", func(t *testing.T) {
+		startTime := time.Now()
+		endTime := startTime.AddDate(0, 0, 2*maxQueryTimeWindowInDays)
+
+		var callCount int
+		var httpServerCalls []httpServerCall
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			callCount += 1
+			reqStartTime, reqEndTime, reqError := parseStartAndEndTime(r)
+			httpServerCalls = append(httpServerCalls, httpServerCall{
+				reqStartTime: reqStartTime,
+				reqEndTime:   reqEndTime,
+				reqError:     reqError,
+			})
+			require.NoError(t, reqError)
+		}))
+		defer server.Close()
+
+		lokiLogsPrv := NewLokiLogsProvider(server.Client(), server.URL)
+
+		_, _ = lokiLogsPrv.getValuesOfLabel(context.TODO(), startTime, endTime, "aKind")
+
+		assert.Equal(t, 2, callCount)
+
+		assert.Equal(t, startTime.UnixNano(), httpServerCalls[0].reqStartTime)
+		assert.Equal(t, startTime.AddDate(0, 0, maxQueryTimeWindowInDays).UnixNano(), httpServerCalls[0].reqEndTime)
+
+		assert.Equal(t, startTime.AddDate(0, 0, maxQueryTimeWindowInDays).UnixNano(), httpServerCalls[1].reqStartTime)
+		assert.Equal(t, startTime.AddDate(0, 0, 2*maxQueryTimeWindowInDays).UnixNano(), httpServerCalls[1].reqEndTime)
+	})
+}
+
+func TestNextTimeWindow(t *testing.T) {
+	t.Run("should calculate one time window if startTime + maxTimeWindow == maxEndTime", func(t *testing.T) {
+		startTime := time.Now()
+		var maxTimeWindowInDays = 20
+		maxEndTime := startTime.AddDate(0, 0, 20)
+
+		nextStart, nextEnd, hasNext := nextTimeWindow(startTime.UnixNano(), maxEndTime.UnixNano(), maxTimeWindowInDays)
+
+		assert.Equal(t, startTime.UnixNano(), nextStart)
+		assert.Equal(t, maxEndTime.UnixNano(), nextEnd)
+		assert.False(t, hasNext)
+	})
+
+	t.Run("should calculate more than one time window if startTime + maxTimeWindow < maxEndTime", func(t *testing.T) {
+		startTime := time.Now()
+		var maxTimeWindowInDays = 20
+		maxEndTime := startTime.AddDate(0, 0, 40)
+
+		nextStart, nextEnd, hasNext := nextTimeWindow(startTime.UnixNano(), maxEndTime.UnixNano(), maxTimeWindowInDays)
+
+		assert.Equal(t, startTime.UnixNano(), nextStart)
+		assert.Equal(t, startTime.AddDate(0, 0, maxTimeWindowInDays).UnixNano(), nextEnd)
+		assert.True(t, hasNext)
+	})
+
+	t.Run("should calculate two time windows if startTime + 2 * maxTimeWindow == maxEndTime", func(t *testing.T) {
+		startTime := time.Now()
+		var maxTimeWindowInDays = 24
+		maxEndTime := startTime.AddDate(0, 0, 2*maxTimeWindowInDays)
+
+		nextStart1, nextEnd1, hasNext1 := nextTimeWindow(startTime.UnixNano(), maxEndTime.UnixNano(), maxTimeWindowInDays)
+		nextStart2, nextEnd2, hasNext2 := nextTimeWindow(nextEnd1, maxEndTime.UnixNano(), maxTimeWindowInDays)
+
+		assert.Equal(t, startTime.UnixNano(), nextStart1)
+		assert.Equal(t, startTime.AddDate(0, 0, maxTimeWindowInDays).UnixNano(), nextEnd1)
+		assert.True(t, hasNext1)
+
+		assert.Equal(t, startTime.AddDate(0, 0, maxTimeWindowInDays).UnixNano(), nextStart2)
+		assert.Equal(t, maxEndTime.UnixNano(), nextEnd2)
+		assert.False(t, hasNext2)
+	})
+
+	t.Run("should calculate two time windows where the second ends at maxEndTime if maxEndTime is inside the second time window", func(t *testing.T) {
+		startTime := time.Now()
+		var maxTimeWindowInDays = 20
+		maxEndTime := startTime.AddDate(0, 0, 2*maxTimeWindowInDays-maxTimeWindowInDays/2)
+
+		nextStart1, nextEnd1, hasNext1 := nextTimeWindow(startTime.UnixNano(), maxEndTime.UnixNano(), maxTimeWindowInDays)
+		nextStart2, nextEnd2, hasNext2 := nextTimeWindow(nextEnd1, maxEndTime.UnixNano(), maxTimeWindowInDays)
+
+		assert.Equal(t, startTime.UnixNano(), nextStart1)
+		assert.Equal(t, startTime.AddDate(0, 0, maxTimeWindowInDays).UnixNano(), nextEnd1)
+		assert.True(t, hasNext1)
+
+		assert.Equal(t, startTime.AddDate(0, 0, maxTimeWindowInDays).UnixNano(), nextStart2)
+		assert.Equal(t, maxEndTime.UnixNano(), nextEnd2)
+		assert.False(t, hasNext2)
+	})
+}
+
+func parseStartAndEndTime(r *http.Request) (int64, int64, error) {
+	start, err := strconv.ParseInt(r.URL.Query().Get("start"), 10, 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	end, err := strconv.ParseInt(r.URL.Query().Get("end"), 10, 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	return start, end, nil
 }
